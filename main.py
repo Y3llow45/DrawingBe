1257d39c469db83d35d9344723d684f2b4cebb37
Borky <68009977+Y3llow45@users.noreply.github.com>
2023-01-13 19:51:02 +0200

    Update main.py and add some examples

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from turtle import *
 import argparse
 
 parser = argparse.ArgumentParser(description="Arguments",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-# add argument for colors
-# add argument for setup
-# setup(width=1920, height=1080, startx=0, starty=0)
-parser.add_argument("-r", "--rotate", required=True, default="cw", help="rotate clockwise or counterclockwise")
-parser.add_argument("-s", "--size", required=True, default="d", help="Line size and thickness. Small (s) default (d) big (b)")
 parser.add_argument("-np", "--number_of_operations", required=True, default="5", type=int, help="Number of operations. Recommended 2 to 12 operations.")
 args = parser.parse_args()
 #config = vars(args)
@@ -16,50 +11,18 @@ title("DrawingBe")
 color("purple", "white")
 pensize(2)
 speed(0)
-bgcolor("black")#196 500
-actions = ["left(90)", "forward(20)"]
+bgcolor("black")
+#actions = ["lt(90), fd(30), rt(20), fd(55), lt(200), fd(40), lt(45), fd(70), lt(33), fd(50), rt(120), fd(30), lt(30), fd(40), lt(20), fd(100), lt(50), fd(50), lt(140), fd(80)"]
+#actions = ["fd(100), lt(10), fd(20)"]
+#actions = ["lt(10), fd(60), lt(30), fd(50), lt(100), fd(40), rt(60), fd(30), rt(40), fd(100), lt(34), fd(44), lt(76), fd(56), lt(27), fd(25), rt(15), fd(48)"]
+#setpos(-60,-100)
+#actions = ["fd(10), rt(90), fd(20), rt(120), fd(40), rt(70), fd(50), lt(120), fd(70), rt(40), fd(30), rt(55), fd(45), rt(130), fd(67)"]
+actions = ["fd(50), rt(30), fd(70), lt(34), fd(30), rt(210), fd(61)"]
 
-#for i in range(args.number_of_operations):
-#for h in range(len(actions)):
-    #exec(actions[h])
-#right(180)
 for i in range(args.number_of_operations):
-    print("Now will run: "+str(actions))
     for h in range(len(actions)):
-        if "right" in actions[h]:
-            x = actions[h].replace("right(", "")
-            parsed_number = x.replace(")", "")
-            #print("left({})".format(parsed_number))
-            actions.append("left({})".format(parsed_number))
-            exec("left({})".format(parsed_number))
-        if "left" in actions[h]:
-            x = actions[h].replace("left(", "")
-            parsed_number = x.replace(")", "")
-            #print("right({})".format(parsed_number))
-            actions.append("right({})".format(parsed_number))
-            exec("right({})".format(parsed_number))
-        else:
-            actions.append(actions[h])
-            exec(actions[h])        
-    
-#right(180)
-#for h in range(len(actions)):
-    #if "right" in actions[h]:
-        #x = actions[h].replace("right(", "")
-        #parsed_number = x.replace(")", "")
-        #print("left({})".format(parsed_number))
-        #exec("left({})".format(parsed_number))
-    #if "left" in actions[h]:
-        #x = actions[h].replace("left(", "")
-        #parsed_number = x.replace(")", "")
-        #print("right({})".format(parsed_number))
-        #exec("right({})".format(parsed_number))
-    #else:
-        #exec(actions[h])
-#Notes:
-#6.record action/s in a list
-#7.rotate
-#8.draw actions backwards from the list
-#9.repeat 6/7/8 number times
-#10. sleep 3 seconds and close
+        circle(100,180)
+        exec(actions[h])
+        circle(70,120)
+          
 done()
